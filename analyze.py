bbebc5152e497399bd02cf0e82aa91226fd5888b
Aaryan Lalwani <alalwani@andrew.cmu.edu>
2025-08-29 11:32:07 -0400

    v1

--- analyze.py
+++ analyze.py
@@ -1,12 +1,23 @@
 from google import genai
-from PIL import Image
-import io
+from google.genai import types
 import os
+from dotenv import load_dotenv
 
-gemini_api_key = 'HARDCODED_GEMINI_API_KEY_HERE'
+
+load_dotenv()
+
+gemini_api_key = os.getenv("gemini_api_key")
 gemini_client = genai.Client(api_key=gemini_api_key)
 
-def get_llm_response(image_data: bytes) -> str:
-    image = Image.open(io.BytesIO(image_data))
-    # implement the call to the Gemini API here
-    # docs: https://ai.google.dev/gemini-api/docs/text-generation
+def get_llm_response(image_bytes: bytes, mime_type="image/jpeg") -> str:
+    response = gemini_client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=[
+            types.Part.from_bytes(
+                data=image_bytes,
+                mime_type=mime_type,
+            ),
+            "Caption this image."
+        ]
+    )
+    return response.text
